14b9c3ed39f96306712c38f2cb97f3d6dcc5abd6
Kevin Huang <kevwhuang@gmail.com>
2024-02-13 22:46:20 -0600

    Rehauled base and implemented routes

--- src/layouts/Display.tsx
+++ src/layouts/Display.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Footer from '../components/modules/Footer';
+import Navbar from '../components/modules/Navbar';
+
 function Display(): React.ReactElement {
     return (
         <div id="display">
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 }
