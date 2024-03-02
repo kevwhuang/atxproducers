cc2176247be16ecb4bbc1fea70d033c89e20348b
Thomas Flanigan <tflanig1@gmail.com>
2024-03-01 19:09:53 -0600

    Use .env and add example

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
