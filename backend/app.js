2d72d37c1f76914b63084c1804d18ad23a25ec03
Thomas Flanigan <tflanig1@gmail.com>
2024-04-16 14:14:35 -0500

    DRYer code

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 const port = process.env.PORT || 5000;
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
@@ -12,99 +14,46 @@ const cluster = process.env.MONGO_CLUSTER;
 const api_args = process.env.MONGO_ARGS || 'retryWrites=true&w=majority';
 
 const mongo_uri = `mongodb+srv://${user}:${password}@${cluster}/?${api_args}`
-
 const client = new MongoClient(mongo_uri);
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-});
+async function findDocumentById(collectionName, id, res) {
+    try {
+        const database = client.db('atxproducers');
+        const collection = database.collection(collectionName);
+        const query = { _id: id };
+        const document = await collection.findOne(query);
+
+        if (document) {
+            res.json(document);
+        } else {
+            res.status(404).send(`${collectionName.slice(0, -1)} ${id} not found`);
+        }
+    } catch {
+        console.error(error);
+        res.status(500).send(error);
+    }
+}
 
 app.get('/meetups/:id', async (req, res) => {
-    const eventId = req.params.id;
-    try {
-        await client.connect();
-        const database = client.db('atxproducers');
-        const meetups = database.collection('meetups');
-        const query = { _id: eventId };
-        const meetup = await meetups.findOne(query);
-
-        if (meetup) {
-            res.json(meetup);
-        } else {
-            res.status(404).send('Meetup not found');
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error connecting to the database');
-    }
+    findDocumentById('meetups', req.params.id, res);
 });
 
 app.get('/producers/:id', async (req, res) => {
-    const producerId = req.params.id;
-    try {
-        await client.connect();
-        const database = client.db('atxproducers');
-        const producers = database.collection('producers');
-        const query = { _id: producerId };
-        const producer = await producers.findOne(query);
-
-        if (producer) {
-            res.json(producer);
-        } else {
-            res.status(404).send('Producer not found');
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error connecting to the database');
-    }
+    findDocumentById('producers', req.params.id, res);
 });
 
 app.get('/resources/:id', async (req, res) => {
-    const resourceId = req.params.id;
-    try {
-        await client.connect();
-        const database = client.db('atxproducers');
-        const resources = database.collection('resources');
-        const query = { _id: resourceId };
-        const resource = await resources.findOne(query);
-
-        if (resource) {
-            res.json(resource);
-        } else {
-            res.status(404).send('Resource not found');
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error connecting to the database');
-    }
+    findDocumentById('resources', req.params.id, res);
 });
 
 app.get('/submissions/:id', async (req, res) => {
-    const submissionId = req.params.id;
-    try {
-        await client.connect();
-        const database = client.db('atxproducers');
-        const submissions = database.collection('submissions');
-        const query = { _id: submissionId };
-        const submission = await submissions.findOne(query);
-
-        if (submission) {
-            res.json(submission);
-        } else {
-            res.status(404).send('Submission not found');
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error connecting to the database');
-    }
+    findDocumentById('submissions', req.params.id, res);
 });
 
 app.post('/producers', async (req, res) => {
     try {
-        await client.connect();
         const database = client.db('atxproducers');
         const producers = database.collection('producers');
-
         const producerData = req.body;
         const result = await producers.insertOne(producerData);
 
@@ -115,6 +64,21 @@ app.post('/producers', async (req, res) => {
         }
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send(error);
     }
 });
+
+async function startServer() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB')
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+        process.exit(1);
+    }
+}
+
+startServer();
